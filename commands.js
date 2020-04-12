2f45d9bf054f5a90b58b51d590f7a104848dbb36
Casey Smith <caseysmith@Caseys-MacBook-Pro.local>
2020-04-12 18:05:54 -0400

    basic fetch working for actor and director

--- commands.js
+++ commands.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 const program = require('commander')
-const { getActor, getDirector, getTitle } = require('./index');
+const { getActor, 
+    // getDirector, 
+    // getTitle 
+} = require('./index');
 
 program
 .version('1.0.0')
 .description('Movie DB Command Line Application')
 
 // Add program.commands below
+
+//Get Actor 
+program
+.command("getActor <actor>")
+.description('See what movies an actor has acted in.')
+.action(actor => getActor(actor));
+
+//Get Director 
+program
+.command("getDirector <director>")
+.description('See what movies a director has directed.')
+.action(director => getActor(director));
 
 program.parse(process.argv)
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const fetch = require("node-fetch");
 const API_KEY = "3cfc468f439e1b2dd2e75bb0dfe05e18";
+const searchPersonURL = 'https://api.themoviedb.org/3/search/person?api_key='
 
 //getActor
+const getActor = async (actor) => {
+    console.log(actor)
+    const response = await fetch(`${searchPersonURL}${API_KEY}&language=en-US&query=${actor}&page=1&include_adult=false`);
+    const data = await response.json();
+    const knownForList = data.results[0].known_for
+    const movieList = knownForList.map(movie => movie.title)
+    console.log(movieList)
+}; 
 
 //getDirector
+const getDirector = async (director) => {
+    console.log(director)
+    const response = await fetch(`${searchPersonURL}${API_KEY}&language=en-US&query=${director}&page=1&include_adult=false`);
+    const data = await response.json();
+    const knownForList = data.results[0].known_for
+    const movieList = knownForList.map(movie => movie.title)
+    console.log(movieList)
+}; 
 
 //getTitle
 
 module.exports = {
     getActor,
     getDirector,
-    getTitle
+    // getTitle
   }
